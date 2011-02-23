b5cc416e8b5b6694f9d926b0a64f048e193284a7
Andrew Nesbitt <andrewnez@gmail.com>
2011-02-23 18:30:06 +0000

    Refactored cap task code into DeployTracker module

--- lib/deploy_tracker.rb
+++ lib/deploy_tracker.rb
@@ -1,3 +1,25 @@
+require 'net/https'
+
 module DeployTracker
-  
+  USE_SSL = true
+  DEPLOY_TRACKING_HOST = 'deploytracking.heroku.com'
+  DEPLOY_TRACKING_PATH = '/deploys'
+  DEPLOY_TRACKING_PORT = 443
+
+  def self.notify(api_key, data)
+    puts "[DeployTracker] Tracking Deployment to #{DEPLOY_TRACKING_HOST}"
+
+    params = {'api_key' => api_key}
+    data.each {|k,v| params["deploy[#{k}]"] = v }
+
+    http = Net::HTTP.new(DEPLOY_TRACKING_HOST, DEPLOY_TRACKING_PORT)
+    http.use_ssl = USE_SSL
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Post.new(DEPLOY_TRACKING_PATH)
+    request.set_form_data(params)
+    response = http.request(request)
+
+    throw "[DeployTracker] Error posting to server." unless response.is_a?(Net::HTTPSuccess)
+    puts "[DeployTracker] Deployment tracked"
+  end
 end
--- lib/deploy_tracker/capistrano.rb
+++ lib/deploy_tracker/capistrano.rb
@@ -1,14 +1,3 @@
-require 'net/https'
-# hack to eliminate the SSL certificate verification notification
-class Net::HTTP
-  alias_method :old_initialize, :initialize
-  def initialize(*args)
-    old_initialize(*args)
-    @ssl_context = OpenSSL::SSL::SSLContext.new
-    @ssl_context.verify_mode = OpenSSL::SSL::VERIFY_NONE
-  end
-end
-
 Capistrano::Configuration.instance(:must_exist).load do
   after "deploy",            "deploy:track"
   after "deploy:migrations", "deploy:track"
@@ -16,32 +5,16 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :deploy do
     desc "Send deployment to deploytracker.com"
     task :track, :except => { :no_release => true } do
-      rails_env = fetch(:rails_env, "production")
-      executable = RUBY_PLATFORM.downcase.include?('mswin') ? 'rake.bat' : 'rake'
+      data = {}
+      data['user_email']      = fetch(:email, `git config --get user.email`.chomp)
+      data['github_username'] = fetch(:github_username, `git config --get github.user`.chomp)
+      data['environment']     = fetch(:rails_env, "production")
+      data['revision']        = fetch(:current_revision)
+      data['repository']      = fetch(:repository)
+      data['source']          = fetch(:source, 'capistrano')
+      data['branch']          = fetch(:branch, 'master')
 
-      puts "[DeployTracker] Tracking Deployment"
-
-      email = `git config --get user.email`.chomp
-      throw "[DeployTracker] Please set your email with...\n\t`git config --global user.email YOUR_EMAIL_ADDRESS`." if email.size==0
-      github_username = `git config --get github.user`.chomp
-      throw "[DeployTracker] Please set your github username with...\n\t`git config --global github.user YOUR_GITHUB_USERNAME`." if github_username.size==0
-
-      data = "deploy[github_username]=#{github_username}&deploy[environment]=#{rails_env}&deploy[revision]=#{current_revision}&deploy[repository]=#{repository}&api_key=#{deploy_tracking_api_key}&deploy[source]=capistrano&deploy[branch]=#{branch}&deploy[user_email]=#{email}"
-
-      http = Net::HTTP.new('deploytracking.heroku.com', 443)
-      http.use_ssl = true
-      path = '/deploys'
-
-      headers = {
-        'Content-Type' => 'application/x-www-form-urlencoded'
-      }
-      resp, data = begin
-                   http.post(path, data, headers)
-                 rescue TimeoutError => e
-                   throw "[DeployTracker] Connection timed out."
-                 end
-      throw "[DeployTracker] Error posting to server." unless resp.is_a?(Net::HTTPSuccess)
-      puts "[DeployTracker] Deployment tracked"
+      DeployTracker.notify(fetch(:deploy_tracking_api_key), data)
     end
   end
 end
